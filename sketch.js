610749e423f5a24e4358343aeaa3395feeaa03eb
owls27 <tzuhyo0927@gmail.com>
2021-10-09 20:04:22 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,44 +21,63 @@ let therion = [{
 
 let randomIndex;
 let animating = false;
-let therion = [];
+let therions = [];
+let imageCounter = 0;
+let button;
+
+function preload() {
+
+  for (let i = 0; i <= 5; i++) {
+    therions[i] = loadImage(`assets/therion_${i}.JPG`)
+  }
+}
 
 function setup() {
   createCanvas(800, 800);
-  background(220);
-  textSize(32);
+  background(200);
+  textSize(36);
+  textFont('Courier new')
+  textAlign(CENTER);
+  textStyle(BOLD);
+  fill(255);
+  imageMode(CENTER);
+  frameRate(8);
 
-  text("click to randomize", 50, 50);
-
-}
-
-function preload(){
-
-  for (let i = 0; i <= 5; i++){
-    therion[i] = loadImage("assets/troll_" + )
-  }
+  text("click to randomize", width / 2, height / 2);
+  button = createButton("click to randomize");
+  button.mousePressed(buttonPressed);
 }
 
 function draw() {
   if (animating == true) {
+    clear();
+    image(therions[imageCounter], width / 2, height / 2);
 
+    if (imageCounter < therions.length - 1) {
+      imageCounter++;
+      console.log(imageCounter);
+    } else {
+      imageCounter = 0;
+    }
   }
 }
 
-function randomizer(){
+function randomizer() {
   animating = false;
-    if (therion[0]) {
-      background(random(200, 255));
-      randomIndex = int(random(therion.length));
-      text(`${therion[randomIndex].name}'s color is ${therion[randomIndex].color}`, 50, 50);
-      therion.splice(randomIndex, 1);
-  } else{
+  if (therion[0]) {
+    // background(random(200, 255));
+    clear();
+    randomIndex = int(random(therion.length));
+    image(random(therions), width / 2, height / 2);
+    text(`${therion[randomIndex].name}'s color is ${therion[randomIndex].color}`, width/2, height - 100);
+    therion.splice(randomIndex, 1);
+  } else {
     background(random(200, 255));
     text("nothing left!", 50, 50);
   }
 }
 
-function mousePressed() {
+function buttonPressed() {
   animating = true;
   setTimeout(randomizer, 2000);
 
